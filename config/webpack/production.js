b5979a20d016dda90345ee394d1eced04fb1cb44
Adam Lui <adam@elonsucks.org>
2023-08-15 23:31:29 -0700

    Moved `cache` into `compressionOptions` in `new CompressionPlugin`

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -33,7 +33,7 @@ module.exports = merge(sharedConfig, {
   plugins: [
     new CompressionPlugin({
       filename: '[path][base].gz[query]',
-      cache: true,
+      compressionOptions: { cache: true },
       test: /\.(js|css|html|json|ico|svg|eot|otf|ttf|map)$/,
     }),
     new BundleAnalyzerPlugin({ // generates report.html
